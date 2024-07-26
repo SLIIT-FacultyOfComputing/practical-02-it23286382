bf76e11289fa92fc1542f950d4a18b9c4ee7ed86
it23286382 <it23286382@my.sliit.lk>
2024-07-26 13:06:43 +0530

    task 01 completed

--- lab02/task01.java
+++ lab02/task01.java
@@ -1,8 +1,10 @@
 package lab02;
+import java.util.Arrays;
+import java.io.*;
 import java.util.Scanner;
 class task01{
         public static void main(String[] Args){
-                        Scanner scanner = new scanner(System.in);
+                        Scanner scanner = new Scanner(System.in);
                         System.out.println("enter the firstname");
                         String firstName = scanner.nextLine();
                         System.out.println("enter middle name");
@@ -11,25 +13,59 @@ class task01{
                         String lastName = scanner.nextLine();
 
                         StringBuilder stringbuilder = new StringBuilder();
-                        stringbuilder.append(firstname);
+	                stringbuilder.append(lastName);
+
+			stringbuilder.append(firstName);
                         stringbuilder.append(middleName);
-                        stringbuilder.append(lastName);
+                        
                         String result =  stringbuilder.toString();
 
                         System.out.println("enter a fullname:");
                         String fullName = scanner.nextLine();
 
                         if(result==fullName){
-                        System.out.println("names are identical")
+                        System.out.println("names are identical");
                         }else{
-                        System.out.println("different names")
+                        System.out.println("different names");
                         };
 
+			StringBuilder replacedString = new StringBuilder();
+			char e = 'e';
+			char a = 'a';
+
+			for(char c : result.toCharArray()){
+				if(c==e){
+					replacedString.append("3");
+				}else if(c==a){
+					replacedString.append("@");
+				}else{
+					replacedString.append(c);
+				}
+
+			}
 
 
+			String upperCaseResult = replacedString.toString().toUpperCase();
+			System.out.println(upperCaseResult);
+			
 
+			String[] parts= result.split(" ");
+			System.out.println(Arrays.toString(parts));
 
+			System.out.print("enter a string with leading and trailing spaces :");
+			String spacedLine = scanner.nextLine();
+			String trimmedString = spacedLine.trim();
+			System.out.println(trimmedString);
 
+			String vowels = "aeiouAEIOU";
+			int vowelCount = 0;
+			for(char c:result.toCharArray()){
+				if(vowels.indexOf(c) != -1){
+					vowelCount++;	
+				}
+			}
+
+			System.out.print("number of vowels: "+vowelCount);
 
 
                 }
